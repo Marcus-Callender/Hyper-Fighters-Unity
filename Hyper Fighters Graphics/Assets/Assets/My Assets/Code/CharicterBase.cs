f97d4631a4b47f63add03b8465a01d547c843641
Marcus Callender <m.shute@outlook.com>
2017-02-15 12:22:01 +0000

    Added simple movement code to charicters to test camera tracking
    Light and heavy attacks will move the charicters

--- Hyper Fighters Graphics/Assets/Assets/My Assets/Code/CharicterBase.cs
+++ Hyper Fighters Graphics/Assets/Assets/My Assets/Code/CharicterBase.cs
@@ -6,19 +6,19 @@ public class CharicterBase : MonoBehaviour
 {
 	const int m_c_numInputs = 6;
 	KeyCode[] m_inputs = new KeyCode[m_c_numInputs];
-	string[] m_names = new string[m_c_numInputs];
+	string[] m_moveNames = new string[m_c_numInputs];
 
 	
 
 	// Use this for initialization
 	void Start()
 	{
-		m_names[0] = "Light Attack";
-		m_names[1] = "Heavy Attack";
-		m_names[2] = "Throw";
-		m_names[3] = "Block";
-		m_names[4] = "Dodge";
-		m_names[5] = "Hyper Move";
+		m_moveNames[0] = "Light Attack";
+		m_moveNames[1] = "Heavy Attack";
+		m_moveNames[2] = "Throw";
+		m_moveNames[3] = "Block";
+		m_moveNames[4] = "Dodge";
+		m_moveNames[5] = "Hyper Move";
 
 		m_inputs[0] = KeyCode.None;
 		m_inputs[1] = KeyCode.None;
@@ -40,8 +40,8 @@ public class CharicterBase : MonoBehaviour
 		{
 			if (m_inputs[z] == KeyCode.None)
 			{
-				Debug.Log("Please enter a key for " + m_names[z] + ".");
-				// get input
+				Debug.Log("Please enter a key for " + m_moveNames[z] + ".");
+
 				foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
 				{
 					if (Input.GetKeyDown(key))
@@ -59,12 +59,26 @@ public class CharicterBase : MonoBehaviour
 
 	public void DetectKeyPress()
 	{
+		Vector3 pos = this.transform.position;
+
 		for (int z = 0; z < m_c_numInputs; z++)
 		{
 			if (Input.GetKeyDown(m_inputs[z]))
 			{
-				Debug.Log("Key pressed: " + m_names[z]);
+				Debug.Log("Key pressed: " + m_moveNames[z]);
 			}
 		}
+
+		if (Input.GetKey(m_inputs[0]))
+		{
+			pos.x += 2.0f * Time.deltaTime;
+		}
+
+		if (Input.GetKey(m_inputs[1]))
+		{
+			pos.z += 2.0f * Time.deltaTime;
+		}
+
+		this.transform.position = pos;
 	}
 }
