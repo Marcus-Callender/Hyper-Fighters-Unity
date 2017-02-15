14d81e5eeadfec30bdde2b4f87740cc78e158fa6
Marcus Callender <m.shute@outlook.com>
2017-02-15 16:45:34 +0000

    Added classes for combat between the charicters

---
+++ Hyper Fighters Graphics/Assets/Assets/My Assets/Code/FighterData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterData : MonoBehaviour
+{
+	int m_hp;
+	int m_previousHP;
+	int m_maxHP;
+	string m_name;
+	int m_focus;
+	int m_maxFocus;
+	int m_previousFocus;
+	bool m_knockedDown;
+	//void* m_pStatus;
+	//C_Status* m_pStatuses[3];
+
+	void Start()
+	{
+
+	}
+	
+	void Update()
+	{
+
+	}
+}
---
+++ Hyper Fighters Graphics/Assets/Assets/My Assets/Code/Moves/BaseMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum E_MOVE_TYPE
+{
+	L_ATTACK,
+	H_ATTACK,
+	THROW,
+	BLOCK,
+	DODGE
+}
+
+public class BaseMove : MonoBehaviour
+{
+	int m_damage;
+	float m_speed;
+	bool m_knockDown;
+	double m_FocusGain;
+	string m_name;
+	E_MOVE_TYPE m_type;
+	//C_FighterData* m_pMe;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+
+	}
+}
